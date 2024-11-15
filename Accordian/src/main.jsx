ecffea88e600b311d9a2f1f6f6d9522f5b1a1684
Shikhar Khandelwal <skykunnu@gmail.com>
2024-11-15 15:14:36 +0530

    react-accordian

--- Accordian/src/main.jsx
+++ Accordian/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import Accordian from './Accordian'
 
 createRoot(document.getElementById('root')).render(
