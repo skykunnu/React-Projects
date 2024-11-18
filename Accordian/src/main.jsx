8397da4a351cb5993770d4b0252eb7b8b5a24656
Shikhar Khandelwal <skykunnu@gmail.com>
2024-11-18 12:01:42 +0530

    accordian

--- Accordian/src/main.jsx
+++ Accordian/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Accordian from './Accordian'
+import Accordian from './Accordian.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
