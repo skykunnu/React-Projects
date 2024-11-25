492108902458ed7924a79cd1f78439e0d28fe119
Shikhar Khandelwal <skykunnu@gmail.com>
2024-11-25 14:48:11 +0530

    Flip_card

---
+++ Flip_card/src/App.jsx
@@ -0,0 +1,24 @@
+import Cards from './Card'
+function App() {
+  return(
+    <>
+    <div className="wrapper">
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    </div>
+    </>
+  )
+ 
+}
+
+export default App
