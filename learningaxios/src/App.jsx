f22da185f979411539fd98c0dccbd62cca933919
Shikhar Khandelwal <skykunnu@gmail.com>
2024-12-04 16:13:04 +0530

    axios_Learning

---
+++ learningaxios/src/App.jsx
@@ -0,0 +1,27 @@
+//b5d71c1c43993a8847df5ed37cabf130
+import axios from "axios";
+import { useState } from "react";
+function App() {
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+  async function handleClick() {
+    const response = await axios.get('https://api.themoviedb.org/3/search/movie?api_key=b5d71c1c43993a8847df5ed37cabf130&query='+ value);
+    setData(response.data.results)
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Click Me</button>
+      <input type="text" placeholder="Enter Movie Name" value={value} onChange={(e)=>setValue(e.target.value)} />
+      <ul>
+        {data.map((item)=>(
+
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
