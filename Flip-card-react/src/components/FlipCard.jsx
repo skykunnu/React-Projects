7493ffcecda979d5a505bf32f7f3b2e317d596e8
Shikhar Khandelwal <skykunnu@gmail.com>
2024-12-04 14:31:34 +0530

    New

--- Flip-card-react/src/components/FlipCard.jsx
+++ Flip-card-react/src/components/FlipCard.jsx
@@ -1,42 +1,3 @@
-// import React from "react";
-
-// const FlipCard = ({ image, isFlipped, isMatched, onClick }) => {
-//   return (
-//     <div
-//       className={`flip-card bg-transparent w-[22%] h-[150px] my-2 perspective-1000 ${
-//         isMatched ? "pointer-events-none" : ""
-//       }`}
-//       onClick={onClick}
-//     >
-//       <div
-//         className={`flip-card-inner w-full h-full relative transition-transform duration-800
-//           ${isFlipped ? "rotate-y-180" : ""}`}
-//       >
-//         <div className="flip-card-front absolute w-full h-full bg-card-front backface-hidden">
-//           <img
-//             src="/images/questionMark.jpg"
-//             alt="Question Mark"
-//             className="w-full h-full"
-//           />
-//         </div>
-//         <div className="flip-card-back absolute w-full h-full backface-hidden">
-//           <img
-//             src={image}
-//             alt="Card"
-//             className={`w-full h-full border border-gray-100 p-[0.15rem] ${
-//               isMatched ? "matched" : ""
-//             }`}
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FlipCard;
-
-import React from "react";
-
 const FlipCard = ({ image, isFlipped, isMatched, onClick }) => {
   return (
     <div
@@ -51,7 +12,7 @@ const FlipCard = ({ image, isFlipped, isMatched, onClick }) => {
       >
         <div
           className={`flip-card-front absolute w-full h-full bg-card-front ${
-            isFlipped ? "rotate-y-180 " : "rotate-y-0"
+            isFlipped ? "rotate-y-180 hidden" : "rotate-y-0"
           }`}
         >
           <img
