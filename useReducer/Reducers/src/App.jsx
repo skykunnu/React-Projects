c841ebfd2cdf5b72e7d26f064897eeb716323125
Shikhar Khandelwal <skykunnu@gmail.com>
2025-01-06 14:06:16 +0530

    CSS style

--- useReducer/Reducers/src/App.jsx
+++ useReducer/Reducers/src/App.jsx
@@ -1,11 +1,12 @@
 // import Counter from "./Counter"
+import "./App.css"
 import Todolist from "./Todolist"
 function App() {
   return (
-    <>
+    <div className="todo">
     {/* <Counter /> */}
     <Todolist />
-    </>
+    </div>
   )
 }
 
