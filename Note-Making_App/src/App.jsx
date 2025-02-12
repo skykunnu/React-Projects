9a97a13bdee17f652e0d10f29aeea1258dd9d40a
Shikhar Khandelwal <skykunnu@gmail.com>
2025-02-12 18:00:30 +0530

    Note-Making-App

--- Note-Making_App/src/App.jsx
+++ Note-Making_App/src/App.jsx
@@ -2,13 +2,18 @@
 import {useState} from 'react';
 function App() {
 
-  const [message, setMessage]=useState('');
-  const [color,]
   const [allMessage, setAllMessage]=useState([]);
+  const [text, setText]=useState();
+  const [color,setColor]=useState();
+
+
 
 function handleNote(e){
 e.preventDefault();
-const obj={id:Date.now(),Message:message};
+const obj={id:Date.now(),Message:text, Color:color};
+setAllMessage([...allMessage,obj]);
+setText('');
+setColor('');
 
 
 }
@@ -19,13 +24,19 @@ const obj={id:Date.now(),Message:message};
     <div className='mainDiv'>
     <div className='noteDiv1'>
       <form onSubmit={handleNote}>
-     <textarea name="task" className='taskArea' placeholder='Enter your message' value={message} onChange={(e)=>setMessage(e.target.value)} required></textarea>
-     <input type="color" className='colorPalette' value={} />
+     <textarea name="task" className='taskArea' placeholder='Enter your message' value={text} onChange={(e)=>setText(e.target.value)} required></textarea>
+     <input type="color" className='colorPalette' value={color} onChange={(e)=>setColor(e.target.value)} />
      <button className='btn' type='submit'>Add Note</button>
       </form>
     </div>
     <div className='noteDiv2'>
-
+     {allMessage.map((item)=>{
+      return(
+        <div key={item.id} style={{backgroundColor:item.Color}} className='note'>
+          {item.Message}
+        </div>
+      )
+     })}
     </div>
     </div>
   )
